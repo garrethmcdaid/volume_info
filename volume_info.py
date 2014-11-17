b9da9491934e03ed7c6c37149f87c5ce953a92a7
Garreth McDaid <garreth.mcdaid@nightbluefruit.com>
2014-11-17 22:11:45 +0000

    Fixed IOPS calculation

--- volume_info.py
+++ volume_info.py
@@ -18,14 +18,22 @@ total_no_attached = 0
 total_no_available = 0
 total_iops = 0
 total_no_iops = 0
+total_no_non_iops = 0
+
 
 #ARRAYS
 zone = {}
 
 for v in vol:
     total += v.size
-    total_iops = v.iops
     
+    try:
+        v.iops += 1
+        total_iops += v.iops
+        total_no_iops += 1
+    except TypeError:
+        total_no_non_iops += 1
+
     if v.zone not in zone:
         zone[v.zone] = {}
             
@@ -49,11 +57,13 @@ for v in vol:
     	total_no_available += 1
     	zone[v.zone]["available"] += v.size
     	
+print "Region: %s" % (a[1])    	
+    	
 print "Total Volumes: " + "{0:,}".format(len(vol))
 print "Total Volume Size in GB: " + "{0:,}".format(total) + " (" + "{0:,}".format(len(vol)) + " Volumes)"
 print "Total Attached Volume Size in GB: " + "{0:,}".format(total_attached) + " (" + "{0:,}".format(total_no_attached) + " Volumes)"
 print "Total Available Volume Size in GB: " + "{0:,}".format(total_available) + " (" + "{0:,}".format(total_no_available) + " Volumes)"
-print "Total IOPS: " + "{0:,}".format(total_iops) + " (" + "{0:,}".format(total_no_iops) + " Volumes)"
+print "Total Provisioned IOPS: " + "{0:,}".format(total_iops) + " (" + "{0:,}".format(total_no_iops) + " Volumes)"
 
 for z in zone:
 	print z + ": Attached = " + str(zone[z]["attached"]) + ": Available = " + str(zone[z]["available"]) + ": Total = " + str(zone[z]["total"])
